82d3175ef94339e2260ad3ff9aee677f792a6b8c
daviddunn334 <159349529+daviddunn334@users.noreply.github.com>
2024-04-29 21:19:26 -0500

    tailwind is set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
 import './App.css';
 import Employee from './components/Employee';
-
+import { useState } from 'react';
 
 
 function App() {
+  const [role, setRole] = useState('dev');
   const showEmployees = true;
+  let food = 'Cheese';
   return (
     
     <div className="App">
       {showEmployees ?
       <>
-        <Employee name='David' role='Intern' food='pizza'/>
+      <input type='text' onChange={(e) => {
+        console.log(e.target.value);
+        setRole(e.target.value);
+      }}/>
+        <Employee name='David' role={role} food='pizza'/>
         <Employee name='Jackson' role='CEO'/>
-        <Employee name='Kennedy' food='Steak'/>
-        
+        <Employee name='Kennedy' food='Steak' role='Nurse'/>
+        <Employee name='Deuce' role={role} food={food}/>
        </> 
        :
        <p>you cant see the employees</p>
       }
-      
-       
-      
-      
-
-    
     </div>
   );
 }
